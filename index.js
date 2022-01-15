dd402aa6c8b65536bd3b13a9f5e4f775940bbb68
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-15 12:12:25 -0600

    add Manager, Engineer and Intern files

---
+++ index.js
@@ -0,0 +1,20 @@
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+function addIntern() {
+    inquirer.prompt()
+}
+
+
+function addEngineer() {
+    inquirer.prompt()
+}
+
+
+function addManager() {
+    inquirer.prompt()
+}
+
