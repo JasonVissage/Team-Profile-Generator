697064508a9b16dfd95d513a832382e8b27d3414
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-15 20:27:55 -0600

    Add test files and questions in index.js

--- index.js
+++ index.js
@@ -4,17 +4,41 @@ const Engineer = require("./lib/Engineer");
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Name:'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'ID:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email:'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title:'
+    },
+]
+
 function addIntern() {
-    inquirer.prompt()
+    inquirer.prompt(questions)
 }
 
 
 function addEngineer() {
-    inquirer.prompt()
+    inquirer.prompt(questions)
 }
 
 
 function addManager() {
-    inquirer.prompt()
+    inquirer.prompt(questions)
 }
 
