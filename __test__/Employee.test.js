88de5dde0966e6f775bc0e6908aaa2ebab67491e
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-20 21:55:02 -0600

    commiting for askBCS to help

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,8 +1,8 @@
 const Employee = require('../lib/Employee')
 
 test('creates an employee object', () => {
-    const employee = new Employee("Jason", 123, "Jason@email.com")
-    expect(typeof employee.name).toBe("string")
-    expect(typeof employee.id).toBe("number")
-    expect(typeof employee.email).toBe("string")
+    const employee = new Employee('Jason', 123, 'Jason@email.com')
+    expect(employee.name).toBe('Jason')
+    expect(employee.id).toBe(123)
+    expect(employee.email).toBe('Jason@email.com')
 }); 
