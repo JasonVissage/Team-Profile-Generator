6c558faafbea3f261a79783d5a5da362341e018c
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-21 14:34:11 -0600

    fixed Intern test

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,2 +1,9 @@
 const Intern = require('../lib/Intern')
 
+test('creates an intern object', () => {
+    const intern = new Intern('Jason', 123, 'Jason@email.com', 'school')
+    expect(intern.name).toBe('Jason')
+    expect(intern.id).toBe(123)
+    expect(intern.email).toBe('Jason@email.com')
+    expect(intern.school).toBe('school')
+}); 
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,2 +1,9 @@
 const Manager = require('../lib/Manager')
 
+test('creates an manager object', () => {
+    const manager = new Manager('Jason', 123, 'Jason@email.com', 'JasonHub')
+    expect(manager.name).toBe('Jason')
+    expect(manager.id).toBe(123)
+    expect(manager.email).toBe('Jason@email.com')
+    expect(manager.githubuser).toBe('JasonHub')
+}); 
