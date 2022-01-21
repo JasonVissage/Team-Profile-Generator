5b4038ccc0d9e780b9af08dff3c30736030ae323
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-20 22:11:32 -0600

    got employee test working

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,8 +1,8 @@
 const Employee = require('../lib/Employee')
 
 test('creates an employee object', () => {
-    const employee = new Employee('Jason', 123, 'Jason@email.com')
-    expect(employee.name).toBe('Jason')
+    const employee = new Employee(123, 'Jason', 'Jason@email.com')
     expect(employee.id).toBe(123)
+    expect(employee.name).toBe('Jason')
     expect(employee.email).toBe('Jason@email.com')
 }); 
--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,2 +1,9 @@
 const Engineer = require('../lib/Engineer')
 
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Jason', 123, 'Jason@email.com', 'JasonHub')
+    expect(engineer.name).toBe('Jason')
+    expect(engineer.id).toBe(123)
+    expect(engineer.email).toBe('Jason@email.com')
+    expect(engineer.githubuser).toBe('JasonHub')
+}); 
