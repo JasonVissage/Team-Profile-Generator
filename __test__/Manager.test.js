049f4a4bf049891fd51d28dd6738e15748c5436a
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-01-21 14:37:42 -0600

    fixed Manager test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,9 +1,9 @@
 const Manager = require('../lib/Manager')
 
 test('creates an manager object', () => {
-    const manager = new Manager('Jason', 123, 'Jason@email.com', 'JasonHub')
+    const manager = new Manager('Jason', 123, 'Jason@email.com', 'offNumber')
     expect(manager.name).toBe('Jason')
     expect(manager.id).toBe(123)
     expect(manager.email).toBe('Jason@email.com')
-    expect(manager.githubuser).toBe('JasonHub')
+    expect(manager.offNumber).toBe('offNumber')
 }); 
